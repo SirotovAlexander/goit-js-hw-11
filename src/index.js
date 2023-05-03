efc784f23e468974dc36fc44627219e4385b8c13
SirotovAlexander <alexander.sirotov@gmail.com>
2023-05-04 00:58:55 +0300

    create function
    create function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import Notiflix from 'notiflix';
 import { getGallery, totalPages } from './js/apiUse';
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const btnLoad = document.querySelector('.load-btn');
+
+form.addEventListener('change', onInput);
+form.addEventListener('submit', onSubmit);
+btnLoad.addEventListener('click', onClick);
